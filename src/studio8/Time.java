3531df7a3e23502132e3347f5595af0651406e72
EliasBarro <barro@wustl.edu>
2023-04-06 12:02:58 -0500

    time1

--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -2,10 +2,31 @@ package studio8;
 
 
 public class Time {
+private int hour;
+private int minutes;
+private boolean twentyFour;
+
+public Time(int hours, int minutes1, boolean twentyFourOrNot) {
+	hour =hours;
+	minutes=minutes1;
+	twentyFour=twentyFourOrNot;
+}
+public String toString() {
+if (twentyFour)
+	return (hour + ":" + minutes);
+else {
+	if (hour<13)
+		return hour + ":" + minutes + " am";
+	else
+		return hour-12 + ":" + minutes + " pm";
+}
+	
+}
 
 	public static void main(String[] args) {
  
-    	
+    	Time a = new Time(13,32,false);
+    	System.out.print(a);
     }
 
 }
