d12c6afe90a27e6ef02bd13cb0334e2d68c8e1d8
margaretfoley <margaretfoley@localhost>
2023-04-06 12:33:01 -0500

    push!

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,5 +1,7 @@
 package studio8;
 
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Objects;
 
 public class Date {
@@ -41,5 +43,24 @@ public String toString() {
     	System.out.println(a);
     	Date b = new Date(4,6,2023,false);
     	System.out.println(a.equals(b));
+    	Date c = new Date(6,7,2003,true);
+    	Date d = new Date(1,16,2022, true);
+    	Date e = new Date(3,8,2002,true);
+    	LinkedList<Date> list = new LinkedList<Date>();
+    	list.add(a);
+    	list.add(a);
+    	list.add(b);
+    	list.add(c);
+    	list.add(d);
+    	list.add(e);
+    	System.out.println(list);
+    	HashSet<Date> set = new HashSet<Date>();
+    	set.add(a);
+    	set.add(a);
+    	set.add(b);
+    	set.add(c);
+    	set.add(d);
+    	set.add(e);
+    	System.out.println(set);
     }
 }
