e0c2f6b6dbf2c5203937496747b72ff9a4fde57a
Andre <Andre@DESKTOP-8FIDMFL.dhcp.wustl.edu>
2023-04-06 11:56:31 -0500

    finished date

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,27 @@
 package studio8;
 
 public class Date {
-
-
+	private int month;
+	private int day;
+	private int year;
+	private boolean holiday;
+public Date(int month, int day, int year, boolean holiday) {
+	this.month = month;
+	this.day = day;
+	this.year = year;
+	this.holiday = holiday;
+	
+}
+public String toString() {
+	if (holiday == true) {
+		return month+"/"+day+"/"+year + " is a holiday!";
+	}
+	else {
+		return month+"/"+day+"/"+year + " is not a holiday!";
+	}
+}
     public static void main(String[] args) {
-
+    	Date a = new Date (4,6,2023,true);
+    	System.out.print(a);
     }
-
 }
